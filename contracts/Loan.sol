1211c550ce08980eb254027fd61143feb9860499
njx <njx@undercurrent.tech>
2023-02-16 10:55:45 +0800

    some changes on contract dir

--- contracts/Loan.sol
+++ contracts/Loan.sol
@@ -1,119 +1,117 @@
 // SPDX-License-Identifier: unlicensed
-pragma solidity 0.8.1;
+pragma solidity ^0.8.1;
 
-import {MinerAPI} from "https://github.com/Zondax/filecoin-solidity-mock-api/blob/master/contracts/v0.8/MinerAPI.sol";
-import {MinerTypes} from "https://github.com/Zondax/filecoin-solidity-mock-api/blob/master/contracts/v0.8/types/MinerTypes.sol";
-import "./PFSPLoanWallet.sol";
-
-contract LoanPoolContract {
-    struct Funders {
-        address funder;
-        uint256 amount;
-    }
-
-    struct LoanTx {
-        address sp;
-        uint256 amount;
-        uint256 timeStarted;
-    }
-
-    address public minerApiAddress;
-    Funders[] public fundersArr;
-    uint256 public counter = 0;
-    uint256 public maxAmount = 10 ether; // Only 10 just for testing
+contract PFSPLoanWallet {
+    address[] public approvers;
+    address public approver1;
+    address public approver2;
     address public admin;
-    mapping(address => uint256) public funders;
-    mapping(uint256 => LoanTx) public loanTxs;
-    event LoanApplied(address indexed sp, uint256 indexed txIndex, uint256 amount);
-    event Withdraw(address indexed funer, uint256 amount);
+    uint256 public timeCreated;
+    uint256 public quorum;
+    uint256 public lockedPeriod = 180 days;
+    uint256 public counter = 0;
+    uint256 public approver1Percentage = 50;
+    uint256 public approver2Percentage = 50;
+    uint256 public denominator = 100;
+    struct Transfer {
+        uint256 id;
+        uint256 amount;
+        uint256 approvals;
+        bool sent;
+    }
+    mapping(uint256 => Transfer) public transfers;
+    mapping(address => mapping(uint256 => bool)) public approvals;
 
-    constructor(address _minerApiAddress) {
-        admin = msg.sender;
-        minerApiAddress = _minerApiAddress;
+    // approver1 usually is the team, while approver2 is usually the SP
+    constructor(address _approver1, address _approver2) {
+        approvers = [approver1, approver2];
+        admin = _approver1;
+        approver1 = _approver1;
+        approver2 = _approver2;
+        quorum = 2;
+        timeCreated = block.timestamp;
     }
 
-    function funderWithdraw(uint256 _amount) external {
-        require(funders[msg.sender] > 0, "You have not funded");
-        require(_amount <= funders[msg.sender], "Please input a valid amount");
-        require(_amount <= address(this).balance, "Please tell admin to fund the treasury");
-
-        funders[msg.sender] -= _amount;
-
-        for (uint256 i = 0; i < fundersArr.length; i++) {
-            if (fundersArr[i].funder == msg.sender) {
-                fundersArr[i].amount -= _amount;
-            }
-        }
-
-        // TODO: maybe can add a part call the Treasury Contract to add funder or update the funder amount
-
-        address withdrawer = msg.sender;
-        (bool success, ) = withdrawer.call{value: _amount}("");
-        require(success, "tx failed");
-        emit Withdraw(withdrawer, _amount);
-    }
-
-    function applyLoan(uint256 amount) external {
-        require(amount <= maxAmount, "Please loan an amount lower than Max Amount");
-        require(amount <= address(this).balance, "Please loan an amount lower than Max Amount");
-        address applicant = msg.sender;
-
-        loanTxs[counter] = LoanTx(applicant, amount, block.timestamp);
-
-        MinerAPI minerApiInstance = MinerAPI(minerApiAddress);
-        MinerTypes.ChangeBeneficiaryParams memory params;
-        // beneficiary changed to our newly created LoanWallet
-        address newPFSPWallet = address(new PFSPLoanWallet(admin, applicant));
-        params.new_beneficiary = addressToString(newPFSPWallet);
-        params.new_expiration = 180 days;
-        params.new_quota = 90;
-        minerApiInstance.change_beneficiary(params);
-
-        (bool success, ) = applicant.call{value: amount}("");
-        require(success, "tx failed");
-
-        emit LoanApplied(applicant, counter, amount);
+    function createTransfer(uint256 amount) external onlyApprover {
+        require(amount >= 0.001 ether, "Please input amount more than 0.001");
+        require(amount <= address(this).balance, "Not enough funds in the contract yet");
+        transfers[counter] = Transfer(counter, amount, 2, false);
         counter++;
     }
 
-    receive() external payable {
-        // TODO: maybe can add a part call the Treasury Contract to add funder or update the funder amount
-        if (funders[msg.sender] == 0) {
-            funders[msg.sender] = msg.value;
-        } else {
-            funders[msg.sender] += msg.value;
+    function approveTransfer(uint256 id) external onlyApprover {
+        require(transfers[id].sent == false, "transfer has already been sent");
+        require(approvals[msg.sender][id] == false, "cannot approve transfer twice");
+        require(
+            transfers[id].amount <= address(this).balance,
+            "Not enough funds in the contract yet"
+        );
+
+        approvals[msg.sender][id] = true;
+        transfers[id].approvals++;
+
+        if (transfers[id].approvals >= quorum) {
+            transferFund(id);
         }
+    }
 
-        bool funderExist = false;
-        for (uint256 i = 0; i < fundersArr.length; i++) {
-            if (fundersArr[i].funder == msg.sender) {
-                funderExist = true;
-                fundersArr[i].amount += msg.value;
-            }
+    function adminTransferAfterLock(uint256 id) external onlyAdmin {
+        require(timeCreated + lockedPeriod <= block.timestamp);
+        transferFund(id);
+    }
+
+    function transferFund(uint256 id) internal {
+        transfers[id].sent = true;
+        uint256 amount1 = transfers[id].amount * (approver1Percentage / denominator);
+        uint256 amount2 = transfers[id].amount * (approver2Percentage / denominator);
+        //   address payable toApprover1 = payable(approver1);
+        //   address payable toApprover2 = payable(approver2);
+        //   toApprover1.transfer(amount1);
+        //   toApprover2.transfer(amount2);
+
+        // Experiment new way of sending value
+        (bool success1, ) = approver1.call{value: amount1}("");
+        (bool success2, ) = approver2.call{value: amount2}("");
+        require(success1 && success2, "tx failed");
+    }
+
+    function getApprovers() external view returns (address[] memory) {
+        return approvers;
+    }
+
+    function getTransfers(uint256 _id) external view returns (Transfer memory) {
+        return transfers[_id];
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function setApproverPercentage(uint256 _approver1Percentage, uint256 _approver2Percentage)
+        external
+        onlyAdmin
+    {
+        approver1Percentage = _approver1Percentage;
+        approver2Percentage = _approver2Percentage;
+    }
+
+    function setDenominator(uint256 _denominator) external onlyAdmin {
+        denominator = _denominator;
+    }
+
+    modifier onlyApprover() {
+        bool allowed = false;
+        if (msg.sender == approver1 || msg.sender == approver2) {
+            allowed = true;
         }
-
-        if (funderExist == false) fundersArr.push(Funders(msg.sender, msg.value));
+        require(allowed == true, "only approver allowed");
+        _;
     }
 
-    function getFundersAmount(address _id) external view returns (uint256) {
-        return funders[_id];
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Not admin");
+        _;
     }
 
-    function getFundersList() external view returns (Funders[] memory) {
-        return fundersArr;
-    }
-
-    function addressToString(address _addr) public pure returns (string memory) {
-        bytes32 value = bytes32(uint256(uint160(_addr)));
-        bytes memory alphabet = "0123456789abcdef";
-
-        bytes memory str = new bytes(51);
-        str[0] = "0";
-        str[1] = "x";
-        for (uint256 i = 0; i < 20; i++) {
-            str[2 + i * 2] = alphabet[uint8(value[i + 12] >> 4)];
-            str[3 + i * 2] = alphabet[uint8(value[i + 12] & 0x0f)];
-        }
-        return string(str);
-    }
+    receive() external payable {}
 }
